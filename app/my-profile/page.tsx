9e7eac0ef4fdf76766e6851728b0de86dbd3488a
Tanpreet <tanpreetjhally300@gmail.com>
2026-05-04 16:04:15 +0530

    deployment error fixed

--- app/my-profile/page.tsx
+++ app/my-profile/page.tsx
@@ -37,7 +37,7 @@ export default function MyProfile() {
 										<Link href="/">Home <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
 											<path d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z" />
 										</svg> My Profile</Link>
-										<div className="space24" />
+										<div className="space24"/>
 										<h1>My Profile</h1>
 									</div>
 								</div>
